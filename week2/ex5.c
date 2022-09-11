db77340a3426b2f61b103b8f670907e346a283c4
berserk <bulat2020205@gmail.com>
2022-09-11 23:57:30 +0300

    week2: ex5 fixed mistake, while returning the value

--- week2/ex5.c
+++ week2/ex5.c
@@ -4,7 +4,9 @@
 #include <ctype.h>
 
 long long tribonachi(long long n){
-    // 0 1 1 2 4 7 13 24 44 ...
+    // 1 2 3 4 5 6 7  8  9  ... idx
+    // | | | | | | |  |  |   
+    // 0 1 1 2 4 7 13 24 44 ... val
     if (n == 0){
         return 0;
     }
@@ -15,16 +17,16 @@ long long tribonachi(long long n){
     long long v1 = 0;
     long long v2 = 1;
     long long v3 = 1;
-    long long v4 = v1 + v2 + v3;
-
     int times = n - 3;
+
     while(times){
+        long long v4 = v1 + v2 + v3;
         v1 = v2;
         v2 = v3;
         v3 = v4;
-        v4 = v1 + v2 + v3;
         times--;
     }
+    return v3;
 }
 int main(void) {
     printf("trib(4) = %lli\n", tribonachi(4));
