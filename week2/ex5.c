2ac3a9108684bf935ffb93df0fc677cd0507fe57
berserk <bulat2020205@gmail.com>
2022-09-13 21:02:20 +0300

    week2: damn error

--- week2/ex5.c
+++ week2/ex5.c
@@ -17,16 +17,18 @@ long long tribonachi(long long n){
     long long v1 = 0;
     long long v2 = 1;
     long long v3 = 1;
+    long long v4 = v1 + v2 + v3;
+
     int times = n - 3;
 
     while(times){
-        long long v4 = v1 + v2 + v3;
         v1 = v2;
         v2 = v3;
         v3 = v4;
+        v4 = v1 + v2 + v3;
         times--;
     }
-    return v3;
+    return v4;
 }
 int main(void) {
     printf("trib(4) = %lli\n", tribonachi(4));
