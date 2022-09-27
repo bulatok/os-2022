56d1abfdf3ecb11dc9e94c5f095aefe4828b2225
berserk <bulat2020205@gmail.com>
2022-09-27 13:47:18 +0300

    week4: ex3 added pstree in ex3.sh

--- week4/ex3.c
+++ week4/ex3.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]){
     for (int i = 0;i<argc;i++){
         n = atoi(argv[i]);
     }
-
+    sleep(1);
     for (int i = 0; i < n; i++){
         fork();
         sleep(5);
--- week4/ex3.sh
+++ week4/ex3.sh
@@ -1,5 +1,36 @@
 gcc -o ex3 ex3.c;
 
-echo "started with n = 3" && ./ex3 3 
+echo "started with n = 3";
+./ex3 3 &
+pstree
+sleep 1
 
-echo "started with n = 5" && ./ex3 5;
+pstree;
+sleep 5;
+
+pstree;
+sleep 5;
+
+pstree;
+sleep 5;
+
+echo "started with n = 5";
+./ex3 5 &
+
+pstree
+sleep 1
+
+pstree;
+sleep 5;
+
+pstree;
+sleep 5;
+
+pstree;
+sleep 5;
+
+pstree;
+sleep 5;
+
+pstree;
+sleep 5;
